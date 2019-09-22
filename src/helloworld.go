04eec180ff5af1c20b06f9d3df3c93f8e84535f1
yuanyi.lp <yuanyi.lp@alibaba-inc.com>
2019-09-22 21:37:06 +0800

    update display

--- src/helloworld.go
+++ src/helloworld.go
@@ -22,7 +22,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 	rand.Seed(time.Now().Unix())
 
-	fmt.Fprintf(w, "Hello %s! Knative\n", target)
+	fmt.Fprintf(w, "Hello123123123123123123123123123123123123 %s! Knative\n", target)
 
 	if rand.Intn(10) < 4 {
 		l.Error("http handler error")
